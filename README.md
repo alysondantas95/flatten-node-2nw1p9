54ccb88005dac9b66fb3df407c5821114dc70884
alysondantas95 <alyson.als@hotmail.com>
2021-07-28 09:21:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flatten-node-fniwpx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/flatten-node-fniwpx)
---
+++ src/index.js
@@ -0,0 +1,30 @@
+/**
+ * Desafio: escrever uma função que receba um array com valores ou sub-arrays, e retorne todos os valores abertos em um único array
+ * Todos os valores serão compostos por numeros inteiros.
+ *
+ * IMPORTANTE:
+ * Não devem ser utilizadas as funções flat() ou flatMap() dos arrays.
+ * A ideia é avaliar a lógica utilizada, e não a utilização dos métodos prontos.
+ *
+ * Voce pode testar o seu codigo rodando o comando `npm run test` no terminal
+ * e tambem pode alterar o arquivo `index.test.js` se desejar.
+ * Apos enviado, seu codigo sera validado com outros cenarios de teste tambem.
+ *
+ * @example `flatten([1, [2, 3], 4, [5, 6]])` retorna `[1, 2, 3, 4, 5, 6]`
+ * @param numbers array de inteiros ou sub-arrays
+ * @returns novo array, somente com os valores
+ */
+function flatten(array) {
+  // implemente o codigo aqui
+  //
+  var vlrTotal = 0;
+  var numero = [10, 20, 30, 40, 50];
+
+  for (var i = 0; i < numero.length; i++) {
+    vlrTotal += numero[i];
+  }
+  //console.log(vlrTotal);
+  return [console.log(vlrTotal)];
+}
+
+module.exports = flatten;
